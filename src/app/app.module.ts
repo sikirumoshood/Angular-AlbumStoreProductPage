88be10cbfe20794261966bbecd6de1ead752d273
sikirumoshood <sikirumoshood@gmail.com>
2019-10-27 15:08:44 +0100

    Module completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-	declarations: [ AppComponent, ProductPageComponent, ProductDescriptionComponent, ProductTracklistingComponent ],
+	declarations: [ AppComponent, ProductPageComponent, ProductDescriptionComponent, ProductTracklistingComponent, ProductListComponent ],
 	imports: [ BrowserModule, FormsModule, HttpModule ],
 	providers: [ ProductService ],
 	bootstrap: [ AppComponent ]
